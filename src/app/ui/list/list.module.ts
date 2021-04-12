4c6253354850529f12aca62c667cdb185cda6f97
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2021-04-12 16:11:49 -0500

    feat(ui/list): Added Pokemon Component

--- src/app/ui/list/list.module.ts
+++ src/app/ui/list/list.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list/list.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    PokemonComponent
   ],
   imports: [
     CommonModule,
