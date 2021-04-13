9f08416673e6d820094e8389d7a25c0adccff103
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2021-04-12 19:36:00 -0500

    feat(ui/list/components/pokemon): Created view

--- src/app/ui/list/components/pokemon/pokemon.component.ts
+++ src/app/ui/list/components/pokemon/pokemon.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {ApiService} from '../../../../core/services/api/api.service';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent implements OnInit {
+export class PokemonComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() id: number;
+  public pokemon: Pokemon;
+
+  constructor(
+    private api: ApiService
+  ) {
+    this.pokemon = new Pokemon();
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.api.getPokemon(this.id).subscribe((pokemon: any) => {
+      if (pokemon) {
+        this.pokemon = new Pokemon(pokemon);
+      }
+    });
+  }
+
+}
+
+class Pokemon {
+
+  id: number;
+  name: string;
+  types: PokemonType[];
+
+  constructor(pokemon?: Pokemon) {
+    this.id = (pokemon && pokemon.id) ? pokemon.id : null;
+    this.name = (pokemon && pokemon.name) ? pokemon.name : '';
+    this.types = (pokemon && pokemon.types) ? pokemon.types : [];
+  }
+
+}
+
+class PokemonType {
+
+  slot: number;
+  type: { name: string; };
+
+  constructor(type?: PokemonType) {
+    this.slot = (type && type.slot) ? type.slot : null;
+    this.type = (type && type.type) ? type.type : { name: '' };
   }
 
 }
