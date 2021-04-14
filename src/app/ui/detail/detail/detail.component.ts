97c051d4e26d7a4609b3d409fa35271dd7ab49a5
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2021-04-13 19:14:51 -0500

    feat(ui/detail/detail): Added genders

--- src/app/ui/detail/detail/detail.component.ts
+++ src/app/ui/detail/detail/detail.component.ts
@@ -21,6 +21,8 @@ export class DetailComponent implements OnInit, AfterViewInit {
   public species: string[];
   public moves: string[];
   public evolutions: PokemonSpecie[];
+  public genderRates: { gender: 'female' | 'male'; rate: number; }[];
+  private genders: { gender: 'female' | 'male'; rate: number; name: string; }[];
 
   constructor(
     private readonly activeRoute: ActivatedRoute,
@@ -32,12 +34,15 @@ export class DetailComponent implements OnInit, AfterViewInit {
     this.abilities = [];
     this.moves = [];
     this.evolutions = [];
+    this.genders = [];
+    this.genderRates = [];
     this.activeRoute.paramMap.subscribe((param: ParamMap) => {
       this.pokemonId = Number(param.get('pokemonId'));
     });
   }
 
   ngOnInit(): void {
+    this.getGenders();
   }
 
   ngAfterViewInit(): void {
@@ -63,6 +68,36 @@ export class DetailComponent implements OnInit, AfterViewInit {
             this.evolutions.push(new PokemonSpecie(pokemon.species));
           });
         });
+      }
+    });
+    setTimeout(() => {
+      this.getGender(this.pokemon.name);
+    }, 1000);
+  }
+
+  private getGenders(): void {
+    // TODO: This segment code can improve, moving it to core service, stared at APP_INITIALIZE or act like a global store
+    forkJoin([
+      this.api.getGenders('male'),
+      this.api.getGenders('female')
+    ]).subscribe((responses) => {
+      if (responses[0]) {
+        responses[0].pokemon_species_details.forEach((detail: { pokemon_species: { name: string; }, rate: number }) => {
+          this.genders.push({ gender: 'male', name: detail.pokemon_species.name, rate: detail.rate });
+        });
+      }
+      if (responses[1]) {
+        responses[1].pokemon_species_details.forEach((detail: { pokemon_species: { name: string; }, rate: number }) => {
+          this.genders.push({ gender: 'female', name: detail.pokemon_species.name, rate: detail.rate });
+        });
+      }
+    });
+  }
+
+  private getGender(pokemonName: string): void {
+    this.genders.forEach((gender) => {
+      if (pokemonName === gender.name) {
+        this.genderRates.push({ gender: gender.gender, rate: gender.rate });
       }
     });
   }
